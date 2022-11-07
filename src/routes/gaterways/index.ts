e813f2de62cde59b17204420c8537b1f1637dbc6
carlitorweb <crsanchez8646@gmail.com>
2022-11-07 00:41:10 -0500

    new express routes for our endpoints

--- src/routes/gaterways/index.ts
+++ src/routes/gaterways/index.ts
@@ -6,7 +6,16 @@ const gaterwaysRouter = express.Router();
 
 // local controls dependencies
 import createGaterway from '@controllers/gaterway/create';
+import getAllGaterways from '@controllers/gaterway/getAll';
+import getUniqueGaterway from '@controllers/gaterway/getUnique';
 
+// Create a new gaterway endpoint - http://127.0.0.1:8000/gaterways/create
 gaterwaysRouter.route('/create').post(createGaterway);
+
+// Get the information of a single gaterway - http://127.0.0.1:8000/gaterways/gaterwayIdString
+gaterwaysRouter.route('/:id').get(getUniqueGaterway);
+
+// Get all gaterways information endpoint - http://127.0.0.1:8000/gaterways
+gaterwaysRouter.route('/').get(getAllGaterways);
 
 export default gaterwaysRouter;
